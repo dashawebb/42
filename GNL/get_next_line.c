46944bdf9ea86f29c52aa47dbe98627b79afeb46
dashawebb <35106075+dashawebb@users.noreply.github.com>
2019-01-16 00:09:21 +0300

    2019-01-16

--- GNL/get_next_line.c
+++ GNL/get_next_line.c
@@ -1,26 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line-2.c                                  :+:      :+:    :+:   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: creek <marvin@42.fr>                       +#+  +:+       +#+        */
+/*   By: creek <creek@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/08 16:31:43 by creek             #+#    #+#             */
-/*   Updated: 2019/01/08 22:45:38 by creek            ###   ########.fr       */
+/*   Updated: 2019/01/15 23:32:19 by creek            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include "libft.h"
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 int		ft_display_line(const int fd, char **line)
 {
   static char buf[BUFF_SIZE + 1];
+  static char new_buf[BUFF_SIZE + 1];
+  static char new_buf_2[BUFF_SIZE + 1];
   int ret;
+  int len;
   int i;
   int j;
 
@@ -31,20 +36,32 @@ int		ft_display_line(const int fd, char **line)
   {
 	  ret = read(fd, buf, BUFF_SIZE); // в ret передается результат выполнения ф-ии read - кол-во считанных байт
 	  /*дальше надо считать все до \n в line, а в buf оставить оставшууюся часть файла */
-	  if (ret == 0) // < BUFF_SIZE) // значит это конец файла.! но надо все равно оставшуюся часть записать. Ret меньше BUFF_SIZE может быть во многих случаях. БАФФ_сайз может быть равен 9999999, а сам файл - всего 15 символов
+	  if (ret == 0) // < BUFF_SIZE) // значит это конец файла! но надо все равно оставшуюся часть записать. Ret меньше BUFF_SIZE может быть во многих случаях. БАФФ_сайз может быть равен 9999999, а сам файл - всего 15 символов
 		  return (0);
 	  if (ret == -1)
 		  return (-1); // в какой-то момент считывание не удалось, значит мы выходим из ф-ии?*/
-      printf("%s - это - линия до\n", *line);
-	  while (buf[j] != '\n')
+	len = ft_strlen(buf); // output - 60
+	printf("%d\n", len);
+	  while (buf[j] != '\n') // в new_buf здесь записывается только выводимая часть строки
 	  {
-		  // *line = &buf[j];
-		  *line[i] = buf[j];
+		  new_buf[i] = buf[j];
 		  i++;
 		  j++;
 	  }
-	  printf("%s - это - считанная линия", line[0]);
-
+	  i = 0;
+	  j += 1;
+	  while (buf[j]) // в new_buf здесь записывается только выводимая часть строки
+	 {
+		 new_buf_2[i] = buf[j];
+		 i++;
+		 j++;
+	 }
+	  *line = &new_buf[0]; // line теперь содержит только строку до \n
+	  printf("%s - это - считанная линия\n", *line);
+	  printf("%s - это - изначальный буфер\n", &buf[0]); // в буф произошло все считывание. Надо удалить первую часть?
+	  printf("%s - это - буфер для записи остатка\n", &new_buf_2[0]);
+	  // free();
+	  printf("%s - это буфер после освобождения\n", &buf[0]);
 //	  buf[BUFF_SIZE - 1] = '\0';
   }
   return (0);
@@ -60,8 +77,11 @@ int	main(int argc, char **argv)
 	else if (argc > 2)
 		write(1, "Too many arguments.\n", 20);
 	else
-	{ 
+	{
 		fd = open(argv[1], O_RDONLY);
+		ft_display_line(fd, &line);
+		ft_display_line(fd, &line);
+		ft_display_line(fd, &line);
 		ft_display_line(fd, &line);
 	}
 	return (0);
--- GNL/get_next_line.h
+++ GNL/get_next_line.h
@@ -5,15 +5,16 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: creek <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/01/08 16:43:23 by creek             #+#    #+#             */
-/*   Updated: 2019/01/08 16:48:21 by creek            ###   ########.fr       */
+/*   Created: 2019/01/15 20:42:15 by creek             #+#    #+#             */
+/*   Updated: 2019/01/15 20:53:35 by creek            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#include "libft.h"
+#ifndef _GET_NEXT_LINE_H_
+# define _GET_NEXT_LINE_H_
 # define BUFF_SIZE 4096
 
-int		ft_display_line(const int fd, char **line);
+int	ft_display_line(const int fd, char **line);
 
-#endif
+# endif
