0e7a66264822f223941a87484536beba2643b340
Christop Reek <creek@e2r12p4.21-school.ru>
2019-01-20 12:39:45 +0300

    2019_01_20

--- GNL/get_next_line.h
+++ GNL/get_next_line.h
@@ -3,17 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: creek <marvin@42.fr>                       +#+  +:+       +#+        */
+/*   By: creek <creek@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/15 20:42:15 by creek             #+#    #+#             */
-/*   Updated: 2019/01/15 20:53:35 by creek            ###   ########.fr       */
+/*   Updated: 2019/01/16 17:40:20 by creek            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #ifndef _GET_NEXT_LINE_H_
 # define _GET_NEXT_LINE_H_
-# define BUFF_SIZE 4096
+# define BUFF_SIZE 4
 
 int	ft_display_line(const int fd, char **line);
 
