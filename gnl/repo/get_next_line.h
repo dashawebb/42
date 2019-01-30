0fa46118b61ad4608cae3e3152b6ae3e56f7b93b
Christop Reek <creek@e2r12p4.21-school.ru>
2019-01-30 13:53:55 +0300

    3rd commit

---
+++ gnl/repo/get_next_line.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: creek <creek@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/01/15 20:42:15 by creek             #+#    #+#             */
+/*   Updated: 2019/01/26 18:23:18 by creek            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# define BUFF_SIZE 20
+# include <stdlib.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include <unistd.h>
+# include "libft.h"
+
+int	get_next_line(const int fd, char **line);
+
+#endif
