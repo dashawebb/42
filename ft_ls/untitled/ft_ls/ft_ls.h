42161e6be4900439ebcc90c2d43f505945301c1f
Christop Reek <creek@e2r12p4.21-school.ru>
2019-03-10 20:57:15 +0300

    started ft_ls

---
+++ ft_ls/untitled/ft_ls/ft_ls.h
@@ -0,0 +1,8 @@
+//
+// Created by Christop Reek on 2019-03-10.
+//
+
+#ifndef FT_LS_H
+#define FT_LS_H
+
+#endif
---
+++ ft_ls/untitled/main.c
@@ -0,0 +1,132 @@
+#ifndef st_mtime
+#define st_mtime st_mtimespec.tv_sec
+#endif
+
+#include <stdio.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/xattr.h>
+#include <pwd.h>
+#include <uuid/uuid.h>
+#include <grp.h>
+
+int	main()
+{
+    printf("Hello, World!\n");
+    int a;
+
+    a = 5;
+
+    a = 6;
+	DIR *directory;
+	struct dirent *dirent1;
+
+    directory = opendir("./");
+	dirent1 = readdir(directory);
+	closedir(directory);
+
+	char *path;
+	struct stat buf;
+
+	path = "./Makefile";
+//	path = "/Users/creek/42/42_pdfs";
+	a = lstat(path, &buf);
+
+
+	// print name
+	char *name;
+	name = strrchr(path, '/');
+	printf("name: %s\n", ++name);
+	// print attr
+	char *attributes;
+	size_t attr_size = 20; // RANDOM_NUM
+//	getxattr(path, name, attributes, attr_size);
+	printf("attributes: %s\n", attributes);
+	// print size
+	printf("size in bytes: %lld\n", buf.st_size);
+	// print date mod
+	// print date created
+	printf("Access time: %s\n", ctime(&buf.st_atimespec));
+	printf("Modification time: %s\n", ctime(&buf.st_mtimespec));
+	printf("Change time: %s\n", ctime(&buf.st_ctimespec));
+	// print type of file/dir
+	if (S_ISREG(buf.st_mode))
+		printf("type of file/dir: regular file\n");
+	if (S_ISDIR(buf.st_mode))
+		printf("type of file/dir: directory\n");
+	if (S_ISCHR(buf.st_mode))
+		printf("type of file/dir: character device\n");
+	if (S_ISBLK(buf.st_mode))
+		printf("type of file/dir: block device\n");
+	if (S_ISFIFO(buf.st_mode))
+		printf("type of file/dir: FIFO (pipe)\n");
+	if (S_ISLNK(buf.st_mode))
+		printf("type of file/dir: symbolic link\n");
+	if (S_ISSOCK(buf.st_mode))
+		printf("type of file/dir: socket\n");
+	// print serial number
+	printf("serial number: %llu\n", buf.st_ino);
+	// print gid uid
+	struct passwd *pwd;
+	pwd = getpwuid(buf.st_uid);
+	if(pwd == NULL)
+		perror("getpwuid");
+	else
+		printf("uid: %s\n", pwd->pw_name);
+
+	struct group *group_name;
+	group_name = getgrgid(buf.st_gid);
+	if(group_name == NULL)
+		perror("getgrgid");
+	else
+		printf("gid: %s\n", group_name->gr_name);
+
+	// print chmod
+	int statchmod = buf.st_mode & (S_IRWXU | S_IRWXG | S_IRWXO);
+	printf("chmod: %o\n", statchmod);
+
+	//	printf("user defined flags:\n", buf.st_mode);
+	// column 2? in ls -la
+	printf("hard links: %hu\n", buf.st_nlink);
+
+
+
+
+	// get list of files in dir
+	// get list of dirs in dir
+//	dirp = opendir(".");
+//	if (dirp == NULL)
+//		return (ERROR);
+//	len = strlen(name);
+//	while ((dp = readdir(dirp)) != NULL) {
+//		if (dp->d_namlen == len && strcmp(dp->d_name, name) == 0) {
+//			(void)closedir(dirp);
+//			return (FOUND);
+//		}
+//	}
+//	(void)closedir(dirp);
+//	return (NOT_FOUND);
+
+
+//	struct stat { /* when _DARWIN_FEATURE_64_BIT_INODE is NOT defined */
+//		dev_t    st_dev;    /* device inode resides on */
+//		ino_t    st_ino;    /* inode's number */
+//		mode_t   st_mode;   /* inode protection mode */
+//		nlink_t  st_nlink;  /* number of hard links to the file */
+//		uid_t    st_uid;    /* user-id of owner */
+//		gid_t    st_gid;    /* group-id of owner */
+//		dev_t    st_rdev;   /* device type, for special file inode */
+//		struct timespec st_atimespec;  /* time of last access */
+//		struct timespec st_mtimespec;  /* time of last data modification */
+//		struct timespec st_ctimespec;  /* time of last file status change */
+//		off_t    st_size;   /* file size, in bytes */
+//		quad_t   st_blocks; /* blocks allocated for file */
+//		u_long   st_blksize;/* optimal file sys I/O ops blocksize */
+//		u_long   st_flags;  /* user defined flags for file */
+//		u_long   st_gen;    /* file generation number */
+//	};
+	return 0;
+}
